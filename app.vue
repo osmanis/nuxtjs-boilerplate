f8234b1850a9d0455a93709e9e5b65daea89cedf
Osman Ishaq <43759819+osmanis@users.noreply.github.com>
2025-02-14 21:08:30 +0000

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,22 +1,5 @@
 <template>
-  <div>
-    <script setup lang="ts">
-const version = 2 + 1
-</script>
-
-<template>
   <div class="hello">
     Hello Nuxt {{ version }}!
-  </div>
-</template>
-
-<style scoped>
-.hello {
-  font-family: Arial, Helvetica, sans-serif;
-  font-size: 3rem;
-  padding: 2rem;
-}
-</style>
-
   </div>
 </template>
